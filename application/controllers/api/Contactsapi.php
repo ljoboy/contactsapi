1cff59b64c153c6b2a12038c28553fce680e5cae
Jonathan Yombo <jonathanyombo@gmail.com>
2019-06-10 22:56:22 +0200

    [NEW] Api for contacts

---
+++ application/controllers/api/Contactsapi.php
@@ -0,0 +1,171 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Contactsapi extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Contacts_model');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $q = urldecode($this->input->get('q', TRUE));
+        $start = intval($this->input->get('start'));
+        
+        if ($q <> '') {
+            $config['base_url'] = base_url() . 'contacts/index.html?q=' . urlencode($q);
+            $config['first_url'] = base_url() . 'contacts/index.html?q=' . urlencode($q);
+        } else {
+            $config['base_url'] = base_url() . 'contacts/index.html';
+            $config['first_url'] = base_url() . 'contacts/index.html';
+        }
+
+        $config['per_page'] = 10;
+        $config['page_query_string'] = TRUE;
+        $config['total_rows'] = $this->Contacts_model->total_rows($q);
+        $contacts = $this->Contacts_model->get_limit_data($config['per_page'], $start, $q);
+
+		header('Content-Type: application/json');
+        echo json_encode($contacts);
+    }
+
+    public function read($id) 
+    {
+        $row = $this->Contacts_model->get_by_id($id);
+		header('Content-Type: application/json');
+        if ($row) {
+            $data = array(
+				'idContact' => $row->idContact,
+				'nom' => $row->nom,
+				'postnom' => $row->postnom,
+				'prenom' => $row->prenom,
+				'cree_le' => $row->cree_le,
+				'etat' => $row->etat,
+				'genre' => $row->genre,
+				'img_url' => $row->img_url,
+			);
+        } else {
+        	header("HTTP/1.1 404 Not Found");
+           $data = [];
+        }
+		echo json_encode($data);
+    }
+
+    public function create() 
+    {
+        $data = array(
+            'button' => 'Create',
+            'action' => site_url('contacts/create_action'),
+			'idContact' => set_value('idContact'),
+			'nom' => set_value('nom'),
+			'postnom' => set_value('postnom'),
+			'prenom' => set_value('prenom'),
+			'cree_le' => set_value('cree_le'),
+			'etat' => set_value('etat'),
+			'genre' => set_value('genre'),
+			'img_url' => set_value('img_url'),
+		);
+
+		header('Content-Type: application/json');
+        echo json_encode($data);
+    }
+    
+    public function create_action() 
+    {
+
+        $this->_rules();
+		$data = array(
+			'nom' => $this->input->post('nom',TRUE),
+			'postnom' => $this->input->post('postnom',TRUE),
+			'prenom' => $this->input->post('prenom',TRUE),
+			'cree_le' => $this->input->post('cree_le',TRUE),
+			'etat' => $this->input->post('etat',TRUE),
+			'genre' => $this->input->post('genre',TRUE),
+			'img_url' => $this->input->post('img_url',TRUE),
+		);
+		$this->Contacts_model->insert($data);
+
+		header('Content-Type: application/json');
+		header("HTTP/1.1 201 Created");
+		echo json_encode(['success' => true]);
+    }
+    
+    public function update($id) 
+    {
+        $row = $this->Contacts_model->get_by_id($id);
+
+		header('Content-Type: application/json');
+        if ($row) {
+            $data = array(
+                'button' => 'Update',
+                'action' => site_url('contacts/update_action'),
+				'idContact' => set_value('idContact', $row->idContact),
+				'nom' => set_value('nom', $row->nom),
+				'postnom' => set_value('postnom', $row->postnom),
+				'prenom' => set_value('prenom', $row->prenom),
+				'cree_le' => set_value('cree_le', $row->cree_le),
+				'etat' => set_value('etat', $row->etat),
+				'genre' => set_value('genre', $row->genre),
+				'img_url' => set_value('img_url', $row->img_url),
+			);
+            echo json_encode($data);
+        } else {
+            header("HTTP/1.1 404 Not Found");
+            echo json_encode([]);
+        }
+    }
+    
+    public function update_action() 
+    {
+        $this->_rules();
+
+        $data = array(
+			'nom' => $this->input->post('nom',TRUE),
+			'postnom' => $this->input->post('postnom',TRUE),
+			'prenom' => $this->input->post('prenom',TRUE),
+			'cree_le' => $this->input->post('cree_le',TRUE),
+			'etat' => $this->input->post('etat',TRUE),
+			'genre' => $this->input->post('genre',TRUE),
+			'img_url' => $this->input->post('img_url',TRUE),
+	    );
+
+		$this->Contacts_model->update($this->input->post('idContact', TRUE), $data);
+		header('Content-Type: application/json');
+		header("HTTP/1.1  201 Created");
+		echo json_encode(['success' => true]);
+    }
+    
+    public function delete($id) 
+    {
+        $row = $this->Contacts_model->get_by_id($id);
+
+		header('Content-Type: application/json');
+        if ($row) {
+            $this->Contacts_model->delete($id);
+            echo json_encode(['success' => true]);
+        } else {
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(['error' => false]);
+        }
+    }
+
+    public function _rules() 
+    {
+		$this->form_validation->set_rules('nom', 'nom', 'trim|required');
+		$this->form_validation->set_rules('postnom', 'postnom', 'trim|required');
+		$this->form_validation->set_rules('prenom', 'prenom', 'trim|required');
+		$this->form_validation->set_rules('cree_le', 'cree le', 'trim|required');
+		$this->form_validation->set_rules('etat', 'etat', 'trim|required');
+		$this->form_validation->set_rules('genre', 'genre', 'trim|required');
+		$this->form_validation->set_rules('img_url', 'img url', 'trim|required');
+
+		$this->form_validation->set_rules('idContact', 'idContact', 'trim');
+		$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+    }
+
+}
